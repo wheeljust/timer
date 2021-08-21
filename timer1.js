7e7a96611655f5abb2b79750c8b113352c837477
Justin Wheeler <justinwwheeler@gmail.com>
2021-08-21 18:59:12 +0000

    updated comments

--- timer1.js
+++ timer1.js
@@ -8,7 +8,7 @@
 
 const alarms = process.argv.slice(2);
 
-// Process argv takes in arguments as strings. So need to map each element to a number, then filter out anything that is NaN. Lastly, need to sort from lowest interval to highest so the beeps are in order.
+// Process argv takes in arguments as strings. So need to map each element to a number, then filter out anything that is NaN and less than 0. Lastly, need to sort from lowest interval to highest so the beeps are in order.
 
 const args = alarms
   .map(x => Number(x))
@@ -16,7 +16,7 @@ const args = alarms
   .filter(x => x > 0)
   .sort((a, b) => a - b);
 
-// Iterate through the args array and output beep. Skip numbers that are less than 0 by using continue.
+// Iterate through the args array and output beep.
 
 for (const arg of args) {
   setTimeout(() => {
